207699dedc4fbdfd282467e2b0393b9a94e28290
samul-1 <bsamuele00@gmail.com>
2021-09-02 13:15:57 +0200

    added ALLOWED_HOSTS

--- core/settings/base.py
+++ core/settings/base.py
@@ -27,8 +27,7 @@ SECRET_KEY = environ.get("SECRET_KEY", None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
