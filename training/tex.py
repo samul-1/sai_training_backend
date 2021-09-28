416f8dfa531f3d93df88b808bd6f09cec8a85e47
samul-1 <bsamuele00@gmail.com>
2021-09-28 15:17:34 +0200

    fixed tex rendering issue

--- training/tex.py
+++ training/tex.py
@@ -4,38 +4,6 @@ import re
 import subprocess
 
 
-def svg2img(html: str) -> str:
-    """Converts inline svg elements to images.
-
-    This is done as a work around for #75 of WeasyPrint
-    https://github.com/Kozea/WeasyPrint/issues/75
-    """
-
-    SVG_ELEM_RE = re.compile(r"<svg.*?</svg>", flags=re.MULTILINE | re.DOTALL)
-    B64IMG_TMPL = '<img class="inline" src="data:image/svg+xml;base64,{img_text}"/>'
-    SVG_XMLNS = (
-        'xmlns="http://www.w3.org/2000/svg" '
-        + 'xmlns:xlink="http://www.w3.org/1999/xlink" '
-    )
-
-    while True:
-        match = SVG_ELEM_RE.search(html)
-        if match:
-            svg_text = match.group(0)
-            if "xmlns" not in svg_text:
-                svg_text = svg_text.replace("<svg ", "<svg " + SVG_XMLNS)
-            svg_data = svg_text.encode("utf-8")
-            img_b64_data: bytes = base64.standard_b64encode(svg_data)
-            img_b64_text = img_b64_data.decode("utf-8")
-            img_b64_tag = B64IMG_TMPL.format(img_text=img_b64_text)
-            start, end = match.span()
-            html = html[:start] + img_b64_tag + html[end:]
-        else:
-            break
-
-    return html
-
-
 # takes in a text that might contain TeX formulas wrapped between $ or $$ tags,
 # returns the original text unchanged if no $ tags are found, otherwise returns the original text
 # where all the TeX formulas have been converted to svg and substituted with the svg code
@@ -75,7 +43,7 @@ def tex_to_svg(formula):
                 ],
             )
             # strip off the "b'" and "\n'"
-            output_str += svg2img(str(res)[2:-3])
+            output_str += str(res)[2:-3]
             if token[1] == "$":  # close <p> tag
                 output_str += "</p>"
         else:
